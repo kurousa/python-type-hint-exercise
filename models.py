79f625f3c2ac45e4940d5cf790fafe6713451723
Kurousa <r.takahashi.1026@gmail.com>
2025-12-18 06:14:50 +0900

    chapter7 code

--- models.py
+++ models.py
@@ -5,22 +5,31 @@ models.py
 ドメイン固有の ZipCode や API レスポンスの構造に加えて、複数のモジュールで使う Headers もここに配置
 """
 
-from typing import NewType, NotRequired, TypedDict
+from dataclasses import dataclass
+from typing import ClassVar, NewType, NotRequired, TypedDict
 
 ZipCode = NewType("ZipCode", str)
 type Headers = dict[str, str]
 
 
-class AddressInfo(TypedDict):
+@dataclass(frozen=True, slots=True)
+class AddressInfo:
+    API_PATH: ClassVar[str] = "/v1/address"
+    zipcode: str
     prefecture: str
-    city: str
-    town: str
     prefecture_kana: str
+    city: str
     city_kana: str
+    town: str
     town_kana: str
 
-    # FIXME: データとロジックが分離してしまっている
-    # main.pyの_build_full_addressメソッドをクラスのメソッドとして定義すべき
+    def full_address(self) -> str:
+        """郵便番号、都道府県、市区町村、町名を結合した住所を返す"""
+        return self.prefecture + self.city + self.town
+
+    def full_address_kana(self) -> str:
+        """郵便番号、都道府県、市区町村、町名を結合した住所(カナ表記)を返す"""
+        return self.prefecture_kana + self.city_kana + self.town_kana
 
 
 class FormattedAddress(TypedDict):
